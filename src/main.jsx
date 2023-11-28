47541276a43e44fb7685a99f5de00a38523dc2a3
EddMoi06 <145163602+EddMoi06@users.noreply.github.com>
2023-11-28 14:54:40 -0400

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 'Inicio HTML'
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider/>
   </React.StrictMode>,
 )
