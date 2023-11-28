46c83b4708e0e455857bda4166d1dc2cc5a7de17
EddMoi06 <145163602+EddMoi06@users.noreply.github.com>
2023-11-28 15:27:51 -0400

    new file:   src/components/Layout.jsx 	modified:   src/main.jsx 	new file:   src/pages/Index.jsx 	new file:   src/pages/NuevoCliente.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 'Inicio HTML'
+    element: <Layout/>,
+    children:[
+      {
+        index: true,
+        element: <Index/>
+      },
+      {
+        path: "/clientes/nuevo",
+        element: <NuevoCliente/>
+      }
+    ]
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
